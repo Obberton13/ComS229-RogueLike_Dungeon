cfb3a97cab6779b7c7db52cead54326908a3d0e2
Robert Young <ryoung.david@gmail.com>
2015-02-01 14:16:55 -0600

    Added command line switched for saving and loading dungeons. They don't do anything yet

--- main.c
+++ main.c
@@ -6,10 +6,28 @@
 
 #include "generator.h"
 
+#define DUNGEON_X = 160
+#define DUNGEON_Y = 96
+#define MIN_ROOMS = 12
+#define MAX_ROOMS = 30
+#define ROOM_MIN_W = 8
+#define ROOM_MIN_H = 5
+#define ROOM_SEPARATION = 3
+#define MAX_PLACEMENT_ATTEMPTS = 2000
+
+typedef enum mode {
+	MODE_SAVE,
+	MODE_LOAD,
+	MODE_NORMAL,
+} mode_t;
+
+
+
 void printMap(int **map);
 
 int main(int argc, char *argv[])
 {
+	mode_t m = MODE_NORMAL;
 	unsigned int seed = time(NULL);
 	if(argc>1)
 	{
@@ -33,16 +51,38 @@ int main(int argc, char *argv[])
 			}
 			printf("Seed: %d\n", seed);
 		}
+		if(strcmp(argv[1], "--save")==0)
+		{
+			m = SAVE;
+		}
+		if(strcmp(argv[1], "--load")==0)
+		{
+			m = LOAD;
+		}
 	}
 	srand(seed);
 	int** map = (int**) malloc(sizeof(int*)*160);
+	{
 	int x;
 	for(x=0;x<160;x++)
 	{
 		map[x] = (int*) malloc(sizeof(int)*96);
 	}
-	generateDungeon(map);
+	if(m!=LOAD)
+	{
+		generateDungeon(map);
+	}
+	else
+	{
+		//TODO load the dungeon from ~/.rlg229/dungeon
+		printf("Load Mode!\n");
+	}
 	printMap(map);
+	if(m==SAVE)
+	{
+		//TODO Save the dungeon
+		printf("Save Mode!\n");
+	}
 	for(x=0;x<160;x++)
 	{
 		free(map[x]);
